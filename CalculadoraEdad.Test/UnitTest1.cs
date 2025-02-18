826c8b4217bb60c30b8e3bd54ba069cee14426de
Michell <michelllondono776@gmail.com>
2025-02-17 21:35:14 -0500

    Pruebas TDD

---
+++ CalculadoraEdad.Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+using CalculadoraEdad;
+
+namespace CalculadoraEdad.Test
+{
+    public class TestCalculadoraEdad
+    {
+        
+        [Fact]
+        public void CalcularEdad_CalculaCorrectamenteCuandoNoCumplioAnosEsteAno()
+        {
+            var fechaNacimiento = new DateTime(2000, 2, 17);  
+            var fechaActual = new DateTime(2025, 2, 16);      
+            var calculadora = new CalculadoraEdad();         
+
+        
+            var edadCalculada = calculadora.CalcularEdad(fechaNacimiento, fechaActual); 
+
+            
+            Assert.Equal(24, edadCalculada);  
+        }
+        [Fact]
+        public void CalcularEdad_CalculaCorrectamenteCuandoCumplioAnosEsteAno()
+        {
+            
+            var fechaNacimiento = new DateTime(2000, 2, 17);  
+            var fechaActual = new DateTime(2025, 2, 18);      
+            var calculadora = new CalculadoraEdad();         
+        
+
+            var edadCalculada = calculadora.CalcularEdad(fechaNacimiento, fechaActual);  
+
+           
+            Assert.Equal(25, edadCalculada);  
+        }
+    }
+}
